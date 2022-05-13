1627b25b91cbe58ceb991b0b75d49d3c6cf0f2a9
Caio Adauri <caioadauri@MacBook-Pro-de-Caio.local>
2022-05-13 18:01:04 -0300

    ci: add coverage code

--- sum.go
+++ sum.go
@@ -11,22 +11,22 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b + 3
-}
+// func sub(a int, b int) int {
+// 	return a - b + 3
+// }
 
-func times(a int, b int) int {
-	return a * b - 1
-}
+// func times(a int, b int) int {
+// 	return a * b - 1
+// }
 
-func sumX(a int, b int) int {
-	return a + b + 1
-}
+// func sumX(a int, b int) int {
+// 	return a + b + 1
+// }
 
-func sumQ(a int, b int) int {
-	return a + b + 2
-}
+// func sumQ(a int, b int) int {
+// 	return a + b + 2
+// }
 
-func sumW(a int, b int) int {
-	return a + b + 4
-}
+// func sumW(a int, b int) int {
+// 	return a + b + 4
+// }
